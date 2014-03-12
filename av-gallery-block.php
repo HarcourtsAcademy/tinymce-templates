052a6c97f8bc94e7a99ceaa3f92f9754629a899f
Tim Butler <timb20@gmail.com>
2014-03-12 11:30:10 +1000

    Initial commit.

---
+++ av-gallery-block.php
@@ -0,0 +1,25 @@
+<?php
+
+$templatepath = dirname($_SERVER['PHP_SELF']);
+
+?>
+<div class="av-gallery">
+<ul class="av-gallery-presentations">
+<li>
+<h2>Presentations</h2>
+</li>
+<li><a href="#"><img src="<?php echo $templatepath; ?>/video-thumbnail.gif" alt="thumbnail" class="av-gallery-thumbnail" width="132" height="99">PRESENTATION</a></li>
+</ul>
+<ul class="av-gallery-videos">
+<li>
+<h2>Video and Audio</h2>
+</li>
+<li><a href="#"><img src="<?php echo $templatepath; ?>/video-thumbnail.gif" alt="thumbnail" class="av-gallery-thumbnail" width="132" height="99">VIDEO</a></li>
+</ul>
+<ul class="av-gallery-tutorials">
+<li>
+<h2>Tech Tutorials</h2>
+</li>
+<li><a href="#"><img src="<?php echo $templatepath; ?>/video-thumbnail.gif" alt="thumbnail" class="av-gallery-thumbnail" width="132" height="99">TUTORIAL</a></li>
+</ul>
+</div>
---
+++ template_list.php
@@ -0,0 +1,23 @@
+<?php
+
+$templatepath = dirname($_SERVER['PHP_SELF']);
+
+?>
+
+// This list may be created by a server logic page PHP/ASP/ASPX/JSP in some backend system.
+// There templates will be displayed as a dropdown in all media dialog if the "template_external_list_url"
+// option is defined in TinyMCE init.
+
+var tinyMCETemplateList = [
+	// Name, URL, Description
+	["Course summary", "<?php echo $templatepath; ?>/course-summary.html", "Course summary"],
+	["Information box", "<?php echo $templatepath; ?>/information-box.html", "Information box"],
+  ["AV Gallery block", "<?php echo $templatepath; ?>/av-gallery-block.php", "AV Gallery block"],
+	["Library block", "<?php echo $templatepath; ?>/library-block.html", "Library block"],
+	["Simple table", "<?php echo $templatepath; ?>/simple-table.html", "Simple table"],
+	["NZ CE Case Summary", "<?php echo $templatepath; ?>/nz-ce-case-summary.html", "NZ CE Case Summary"],
+    ["NZ CE Congratulations", "<?php echo $templatepath; ?>/nz-ce-congratulations.html", "NZ CE Congratulations"],
+	["NZ CE Extract", "<?php echo $templatepath; ?>/nz-ce-extract.html", "NZ CE Extract"],
+	["NZ CE Information Note", "<?php echo $templatepath; ?>/nz-ce-information-note.html", "NZ CE Information Note"],
+	["NZ CE Web Link", "<?php echo $templatepath; ?>/nz-ce-web-link.html", "NZ CE Web Link"]
+];
