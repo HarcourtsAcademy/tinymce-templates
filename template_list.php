89430b924c0dc409478a56488757aee804b75db8
Tim Butler <timb20@gmail.com>
2014-03-12 15:04:33 +1000

    Added iStart24 Read action.

--- template_list.php
+++ template_list.php
@@ -15,9 +15,15 @@ var tinyMCETemplateList = [
   ["AV Gallery block", "<?php echo $templatepath; ?>/av-gallery-block.php", "AV Gallery block"],
 	["Library block", "<?php echo $templatepath; ?>/library-block.html", "Library block"],
 	["Simple table", "<?php echo $templatepath; ?>/simple-table.html", "Simple table"],
+	["iStart Action Read", "<?php echo $templatepath; ?>/istart/read.html", "iStart Action Read"]
+];
+
+/* Disabled CE templates.
+
 	["NZ CE Case Summary", "<?php echo $templatepath; ?>/nz-ce-case-summary.html", "NZ CE Case Summary"],
     ["NZ CE Congratulations", "<?php echo $templatepath; ?>/nz-ce-congratulations.html", "NZ CE Congratulations"],
 	["NZ CE Extract", "<?php echo $templatepath; ?>/nz-ce-extract.html", "NZ CE Extract"],
 	["NZ CE Information Note", "<?php echo $templatepath; ?>/nz-ce-information-note.html", "NZ CE Information Note"],
 	["NZ CE Web Link", "<?php echo $templatepath; ?>/nz-ce-web-link.html", "NZ CE Web Link"]
-];
+
+*/
